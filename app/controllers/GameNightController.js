becb47b1faecd8b857f080c90017f8b3d69fca98
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-10-02 19:52:26 -0600

    form added

--- app/controllers/GameNightController.js
+++ app/controllers/GameNightController.js
@@ -6,5 +6,13 @@ export class GameNightController {
   constructor() {
     // console.log('Game Night Controller is loaded', AppState.gameNightPlayers)
   }
-  
+
 }
+
+
+
+
+
+
+// TODO write out function
+// addPlayer()
--- app/models/GameNightPlayer.js
+++ app/models/GameNightPlayer.js
@@ -10,18 +10,18 @@ export class GameNightPlayer {
 
   get GameNightPlayerCard() {
     return `
-    <div class="col-12 col-md-4">
-      <div class="bg-secondary">
+    <div class="col-12">
+      < class="">
         <img src="${this.playerImg}" alt="${this.name}" class="img-fluid">
-          <div class="p-3">
-            <p class="fs-2">${this.name}</p>
-            <p class="fs-3">Score: ${this.score}</p>
-            <div>
-              <button onclick="" class="btn btn-danger">-</button>
-              <button onclick="" class="btn btn-success">+</button>
-            </div>
-
+          
+          <p class="fs-2">${this.name}</p>
+          <p class="fs-3">Score: ${this.score}</p>
+          <div>
+            <button onclick="" class="btn btn-danger">-</button>
+            <button onclick="" class="btn btn-success">+</button>
           </div>
+
+          
 
       </div>
     </div>
